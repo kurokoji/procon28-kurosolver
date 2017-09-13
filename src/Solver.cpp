47895d8ad57e25d5ef911d785b93cfc96e97bac6
kurokoji <shu.kakihana@gmail.com>
2017-09-13 16:17:49 +0900

    回転後の図形を列挙出来るようにした

---
+++ src/Solver.cpp
@@ -0,0 +1,102 @@
+#include "Geo.hpp"
+#include "KuroUtil.hpp"
+#include "Piece.hpp"
+#include "Solver.hpp"
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+namespace procon28 {
+Solver::Solver() {}
+Solver::Solver(const std::vector<Piece>& pieces, const Polygon& frame)
+    : pieces(pieces), frame(frame) {
+      auto v = makeRotatePieces(pieces[0]);
+      for (auto&& t : v) {
+        std::cout << t;
+      }
+}
+
+void Solver::SolvePruned() {
+
+}
+
+bool Solver::canRotate(Piece& piece, const long double angle, const Point& org) {
+  piece = rotate(piece, angle, org);
+  const int N = piece.outer().size() - 1;
+
+  for (int i = 0; i < N; ++i) {
+    const Point& p = piece.outer()[i];
+    if (!eq(round(p.x()), p.x()) || !eq(round(p.y()), p.y())) {
+      return false;
+    }
+  }
+
+  for (auto&& p : piece.outer()) {
+    p = Point(round(p.x()), round(p.y()));
+  }
+
+  return true;
+}
+
+std::vector<Piece> Solver::makeRotatePieces(const Piece& piece) {
+  namespace util = kuroutil;
+  std::vector<Piece> ret;
+  ret.emplace_back(piece);
+
+  const size_t segmentN = piece.segments.size();
+  // 座標は左回り
+  // 各辺に対して回転出来るやつを列挙する
+  for (size_t i = 0; i < segmentN; ++i) {
+    const Segment& s = piece.segments[i];
+    const long double orgAngle = get_angle(s);
+    const Point v = to_vec(s);
+    const int len_sq = v.x() * v.x() + v.y() * v.y();
+    const int MAX_X = std::floor(std::sqrt(len_sq));
+
+    for (int x = 0; x <= MAX_X; ++x) {
+      if (util::is_perfect_square(len_sq - x * x)) {
+        const int y = util::sqrt(len_sq - x * x);
+        long double nowAngle;
+        if (v.x() <= 0) {
+          nowAngle = std::atan2(y, x);
+        } else {
+          nowAngle = std::atan2(x, y);
+        }
+        // どれだけ回転するか(radianで与えられる)
+        const double distAngle = orgAngle - nowAngle;
+        Piece res = piece;
+        if (canRotate(res, distAngle, piece.outer()[i])) {
+          // 同じ図形かどうかを判定するために0番目の座標に合わせる
+          const Point& fr = piece.outer().front();
+          res = translate(res, -fr.x(), -fr.y());
+          // すでに同じ図形があったらpushしない
+          if (std::find_if(std::begin(ret), std::end(ret), [&res](const Piece& x) {
+                return bg::equals(x.poly, res.poly);
+              }) == std::end(ret)) {
+            ret.emplace_back(res);
+          }
+        }
+      }
+    }
+  }
+
+  return ret;
+}
+
+};  // namespace procon28
+
+#ifdef DEBUG
+using namespace procon28;
+
+int main() {
+  Piece piece;
+  std::cin >> piece;
+  Polygon frame;
+  std::vector<Piece> po;
+  po.emplace_back(piece);
+  Solver solver(po, frame);
+}
+
+#endif
--- src/Solver.hpp
+++ src/Solver.hpp
@@ -5,19 +5,36 @@
 #include <vector>
 
 #include "Geo.hpp"
-#include "Hole.hpp"
 #include "Piece.hpp"
 
 namespace procon28 {
+const long double EPS = 1e-10;
 class Solver {
-private:
-  std::vector<Piece> pieces;
-  std::vector<Hole> holes;
-
+//private:
 public:
-  Solver() {}
-  Solver(std::vector<Piece>& pieces, std::vector<Hole>& holes) : pieces(pieces), holes(holes) {}
+  std::vector<Piece> pieces;
+  std::vector<std::vector<Piece>> rotatePieces;
+  Polygon frame;
+  bool canRotate(Piece&, const long double, const Point&);
+  bool eq(long double, long double);
+  long double round(long double);
+  std::vector<Piece> makeRotatePieces(const Piece&);
+public:
+  Solver();
+  Solver(const std::vector<Piece>&, const Polygon&);
+  // 枝刈り全探索による解法
+  void SolvePruned();
 };
+
+inline bool Solver::eq(long double lhs, long double rhs) {
+  return std::abs(lhs - rhs) <= EPS;
+}
+
+inline long double Solver::round(long double n) {
+  long double ret = std::round(n);
+  if (ret == -0) ret = 0;
+  return ret;
+}
 }  // namespace procon28
 
 #endif
